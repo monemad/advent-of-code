e10c99d894e6ecc7d42c16d4e084e6e7fc477a1d
Moiz Ahmad <moiznahmad@gmail.com>
2025-12-02 21:58:03 -0800

    valid -> invalid

--- 2025/day_2/day_2_1.py
+++ 2025/day_2/day_2_1.py
@@ -1,5 +1,5 @@
 def main():
-    valid_sum = 0
+    invalid_sum = 0
     id_ranges = open('day_2_input').readline().strip().split(',')
     for id_range in id_ranges:
         id_low, id_high = id_range.split('-')
@@ -7,8 +7,8 @@ def main():
         for id in range(int(id_low), int(id_high)+1):
             id_str = str(id)
             if len(id_str) % 2 == 1: continue
-            if id_str[:len(id_str)//2] == id_str[len(id_str)//2:]: valid_sum += id
-    print(valid_sum)
+            if id_str[:len(id_str)//2] == id_str[len(id_str)//2:]: invalid_sum += id
+    print(invalid_sum)
 
 
 if __name__ == '__main__':
